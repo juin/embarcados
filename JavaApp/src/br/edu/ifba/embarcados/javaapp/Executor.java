9160b0b07289ed98c80bf428cca7e8d739f71d69
manuela1111 <mn2192@yahoo.com.br>
2015-10-30 20:06:54 -0300

    Implementado o uso de Thread   e utilização do padrão observador

--- JavaApp/src/br/edu/ifba/embarcados/javaapp/Executor.java
+++ JavaApp/src/br/edu/ifba/embarcados/javaapp/Executor.java
@@ -1,24 +1,32 @@
 package br.edu.ifba.embarcados.javaapp;
 
-import br.edu.ifba.embarcados.javaapp.conector.FabricaConectores;
-import br.edu.ifba.embarcados.javaapp.conector.IComunicacaoSensores;
+import br.edu.ifba.embarcados.javaapp.asincexec.AsincExec;
+
 
 public class Executor {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		IComunicacaoSensores  conector = FabricaConectores.getConector();
+		AsincExec asinc = 
+				new AsincExec("/dev/ttyACM0");
+		//vamos colocar ao menos um escutador
+		//tem que colocar o listener dentro do asinc para ele usar
+		ListenerAcelerometro listener = 
+				new ListenerAcelerometro();
+		asinc.addListener(listener);
+		//passar o asinc como se fosse uma thread
 		
-		if (conector.iniciar("COM5")==0){//coloca a porta que identificou no arduino
-			while (true){
-				conector.ler();
-				System.out.println("AcelX =" + conector.getAcelX());
-				System.out.println("AcelY =" + conector.getAcelY());
-				System.out.println("AcelZ =" + conector.getAcelZ());
-				
-				Thread.sleep(100);
-			}
+		Thread t = new Thread(asinc);
+		//nesse ponto que a thread começa a rodar em paralelo
+		t.start();
+		//depois dos 10 segundos que usar a thread vai para-la
+		//Essa thread sleep diz respeito ao main a outra thread vai correr normalmente
+		for (int i = 0; i < 10; i++) {
+			Thread.sleep(1000);
 		}
+		asinc.setContinuar(false);
+		//ter certeza q a thread vai parar
+		t.join();
 
 	}
 
